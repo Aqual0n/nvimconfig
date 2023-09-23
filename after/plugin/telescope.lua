672d7d48e078da4d23be151d8247fdd94b0db2a2
Вячеслав Лашкин <v.lashkin@zebrains.team>
2023-09-24 00:17:53 +0400

    feat(remaps): add remaps for some git commands

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,10 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+-- fu for "find usage"
+vim.keymap.set("n", "<leader>fu", "<cmd>Telescope lsp_references<CR>")
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<CR>")
